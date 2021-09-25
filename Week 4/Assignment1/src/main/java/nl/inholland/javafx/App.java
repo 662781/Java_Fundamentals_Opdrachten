0a4bd548ff5d83539944dee7f9701c1ae25e1ec2
662781 <662781@student.inholland.nl>
2021-09-25 18:43:41 +0200

    Update App.java

--- Week 4/Assignment1/src/main/java/nl/inholland/javafx/App.java
+++ Week 4/Assignment1/src/main/java/nl/inholland/javafx/App.java
@@ -1,24 +1,69 @@
 package nl.inholland.javafx;
 
 import javafx.application.Application;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
+import javafx.geometry.Insets;
 import javafx.scene.Scene;
+import javafx.scene.control.Button;
 import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
 import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.GridPane;
 import javafx.stage.Stage;
 
 public class App extends Application {
     @Override
-    public void start(Stage window) throws Exception {
-        window.setHeight(600);
-        window.setWidth(800);
-        window.setTitle("Inholland JavaFX Starter Project");
+    public void start(Stage window) throws Exception
+    {
+        //Set window size and title
+        window.setHeight(150);
+        window.setWidth(300);
+        window.setTitle("Currency Converter");
 
-        BorderPane pane = new BorderPane();
-        Label hello = new Label("Hello World");
-        pane.setCenter(hello);
+        //Create grid and scene (inside of window)
+        GridPane grid = new GridPane();
+        Scene scene = new Scene(grid);
 
-        Scene scene = new Scene(pane);
+        //Create nodes
+        Label lbl_EuroAmount = new Label("Amount \u20ac:");
+        Label lbl_DollarAmount = new Label("Amount $:");
+        Label lbl_Result = new Label();
+        TextField txt_EuroInput = new TextField();
+        Button btn_Convert = new Button("Convert Euro To Dollar");
+
+        //Set layout
+        grid.setPadding(new Insets(10, 10, 10, 10));
+        grid.setHgap(2);
+        grid.setVgap(10);
+
+        //Put nodes in the grid
+        grid.addRow(0, lbl_EuroAmount, txt_EuroInput);
+        grid.add(btn_Convert, 1, 1);
+        grid.addRow(2, lbl_DollarAmount, lbl_Result);
+
+        //Set the scene and show everything
         window.setScene(scene);
         window.show();
+
+        //Retrieve data from text field
+        int amtInEuro = Integer.parseInt(txt_EuroInput.getText());
+
+        //Sets text from label if button is clicked
+        btn_Convert.setOnAction(new EventHandler<ActionEvent>()
+        {
+            @Override
+            public void handle(ActionEvent actionEvent)
+            {
+                lbl_Result.setText(String.valueOf(euroToDollar(amtInEuro)));
+            }
+        });
+
+    }
+
+    //Converts euro amount to dollar amount
+    protected double euroToDollar(int amtInEuro)
+    {
+        return amtInEuro * 1.18;
     }
 }
